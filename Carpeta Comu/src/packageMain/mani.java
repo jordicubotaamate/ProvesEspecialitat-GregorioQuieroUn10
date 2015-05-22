de0dea864ac8fbb1a4bf69764ae26b35329562ae
Jordi Cubota Amate <cubota.jordi.inf2@gmail.com>
2015-05-22 17:14:48 +0200

    Te he puesto acento

--- Carpeta Comu/src/packageMain/mani.java
+++ Carpeta Comu/src/packageMain/mani.java
@@ -4,7 +4,7 @@ public class mani {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mon "); ///Comentario Eric :3
+		System.out.println("Hola món "); ///Comentario Eric :3
 	}
 
 }
